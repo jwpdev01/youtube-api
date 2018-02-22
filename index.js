3496684ee6a47e040d6880aa3efaf5f8d3d31d01
James Pegram <jwpdev01@gmail.com>
2018-02-22 16:27:56 -0500

    added image, channel name, css, and nav arrow ui

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
-function keyWordsearch(){
-        $('#search-button').on('click', function(e) {
-                e.preventDefault();                
-                gapi.client.setApiKey('AIzaSyAFrScEUoPW6ZBrHIvuAeEDsplHAqS_gl0');
-                gapi.client.load('youtube', 'v3', function() {
+const YOUTUBE_CHANNEL_URL = "https://www.youtube.com/channel/";
+const YOUTUBE_API_KEY = "AIzaSyAFrScEUoPW6ZBrHIvuAeEDsplHAqS_gl0";
+
+function keyWordsearch() {
+        $('#search-button').on('click', function (e) {
+                e.preventDefault();
+                gapi.client.setApiKey(YOUTUBE_API_KEY);
+                gapi.client.load('youtube', 'v3', function () {
                         makeRequest();
                 });
-          
+
         });
-          
-        }
-        function makeRequest() {        
-                let fieldName =  '.js-query';
-                getResponse(getRequest(getQueryFieldValue(fieldName), 'snippet, id', 5));
-                clearQueryFieldValue(fieldName);                       
-        }
 
-        function getQueryFieldValue(className) {
-                return $(className).val();
-        }
 
-        function clearQueryFieldValue(className) {
-                $(className).val('');
-                return true;
-        }
+}
 
-        function getRequest(q, paramPart, paramMaxResults) {
-                return gapi.client.youtube.search.list({
-                        q: q,
-                        part: paramPart,
-                        maxResults: paramMaxResults
-                });
-        }
-        
-        function getResponse(request) {
-                request.execute(function(response) {
-                       let searchResults = response.result.items;
-                       searchResults.forEach(item => $('#results').append(`<li>${item.snippet.title}</li>`));
-                        });    
-        }
+function makeRequest() {
+        let fieldName = '.js-query';
+        getResponse(getRequest(getQueryFieldValue(fieldName), 'snippet, id', 6));
+        clearQueryFieldValue(fieldName);
+}
 
-        function updateResults(item, index) {
-                ('#results').append(`${item.snippet.title}`);
-        }
+function getQueryFieldValue(className) {
+        return $(className).val();
+}
 
-        $(keyWordsearch);
+function clearQueryFieldValue(className) {
+        $(className).val('');
+        return true;
+}
+
+function getRequest(q, paramPart, paramMaxResults) {
+
+        return gapi.client.youtube.search.list({
+                q: q,
+                part: paramPart,
+                maxResults: paramMaxResults
+        });
+}
+
+function getResponse(request) {
+        $('#results').empty();
+
+        request.execute(function (response) {
+                let searchResults = response.result.items;
+                let channelId =
+                        searchResults.forEach(item => $('#results').append(`
+                       <row>
+                          <div class='channel col-4'>
+                            <div class='channel-container'>
+                                <div class='img-link'>
+                                  <a href="https://www.youtube.com/watch?v=${item.id.videoId}" target="_blank">
+                                  <img class="thumb" src="${item.snippet.thumbnails.high.url}" alt="No Image Available."></a>
+                                </div>
+                                <div class='title'>${item.snippet.title}</div>
+                                <div class='channel-link'>
+                                  <a href="https://www.youtube.com/channel/${item.snippet.channelId}" target="_blank">Go to Channel</a>
+                                </div>
+                            </div>
+                        </div>
+                       </row>
+                       `));
+        });
+}
+
+function updateResults(item, index) {
+        ('#results').append(`${item.snippet.title}`);
+}
+
+$(keyWordsearch);
