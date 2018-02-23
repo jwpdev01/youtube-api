e98c057adfb079359227cc7261a9334cfd555682
James Pegram <jwpdev01@gmail.com>
2018-02-22 23:52:08 -0500

    partial code for prev and nav. jquery not working

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const YOUTUBE_CHANNEL_URL = "https://www.youtube.com/channel/";
 const YOUTUBE_API_KEY = "AIzaSyAFrScEUoPW6ZBrHIvuAeEDsplHAqS_gl0";
+let pageToken;
 
 function keyWordsearch() {
-        $('#search-button').on('click', function (e) {
-                e.preventDefault();
-                gapi.client.setApiKey(YOUTUBE_API_KEY);
-                gapi.client.load('youtube', 'v3', function () {
-                        makeRequest();
-                });
+        alert('inside keyword search');
+        setupYouTubeSearch();
+}
 
+function setupYouTubeSearch() {
+        $('#search-button').on('click', function (e) {
+                console.log('search button clicked');
+                setupGAPIClient(e);
         });
 
+        $('.js-prev').on('click', function(e) {
+                alert('prev');
+                console.log('prev button clicked');
+                pageToken = prevPageToken;
+                setupGAPIClient(e);
+        });
 
+        $('.js-next').on('click', function(e) {
+                alert('next');
+                console.log('next button clicked');
+                pageToken = nextPageToken;
+                setupGAPIClient(e);
+        });
+}
+
+function setupGAPIClient(e) {
+        e.preventDefault();
+        gapi.client.setApiKey(YOUTUBE_API_KEY);
+        gapi.client.load('youtube', 'v3', function () {
+                makeRequest();
+        });
 }
 
 function makeRequest() {
         let fieldName = '.js-query';
-        getResponse(getRequest(getQueryFieldValue(fieldName), 'snippet, id', 6));
+        getResponse(getRequest(getQueryFieldValue(fieldName), 'snippet, id', 6, pageToken));
         clearQueryFieldValue(fieldName);
 }
 
@@ -29,11 +51,12 @@ function clearQueryFieldValue(className) {
         return true;
 }
 
-function getRequest(q, paramPart, paramMaxResults) {
+function getRequest(q, paramPart, paramMaxResults, paramPageToken) {
 
         return gapi.client.youtube.search.list({
                 q: q,
                 part: paramPart,
+                pageToken: paramPageToken,
                 maxResults: paramMaxResults
         });
 }
@@ -43,8 +66,18 @@ function getResponse(request) {
 
         request.execute(function (response) {
                 let searchResults = response.result.items;
-                let channelId =
-                        searchResults.forEach(item => $('#results').append(`
+                let prevPageToken = response.result.prevPageToken;
+                let nextPageToken = response.result.nextPageToken;
+                console.log(response);
+
+                $('.nav-container').html(`
+                   <button class='nav-button prev js-prev'>Previous</button>
+                   <span class='search-title'><h1>Search Results</h1></span>
+                   <button class='nav-button next js-next'>Next</button>
+                `);
+
+
+                searchResults.forEach(item => $('#results').append(`
                        <row>
                           <div class='channel col-4'>
                             <div class='channel-container'>
